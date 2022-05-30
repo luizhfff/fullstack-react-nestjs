827d4b4b72c9028a24ffbe5af4e2efe5d120afee
Luiz Henrique Ferreira Facanha Filho <luizfffca@gmail.com>
2022-05-30 05:25:42 +0000

    init front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios'
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if (users.length === 0) {
+      axios.get('https://5000-luizhfff-fullstackreact-w7j5adl4t9s.ws-us46.gitpod.io/user').then(data => setUsers(data.data))
+      console.log(users)
+    }
+  }, [users])
+
+  const removeElement = (id:number) => {
+    axios.delete('https://5000-luizhfff-fullstackreact-w7j5adl4t9s.ws-us46.gitpod.io/user', { data: { id: id } })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test
+      {users && users.map((user: any,index: number) => (
+        <>
+          <div key={index}>{`${user.firstName} ${user.lastName}`}</div>
+          <button onClick={() => removeElement(index)}>Delete</button>
+        </>
+
+      ))}
     </div>
   );
 }
